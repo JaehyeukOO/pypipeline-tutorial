9dce1c80ea350fda39e6f30dfb7b691677c4c60f
Jacob Oh <jaehyeuk.oh@hpcnt.com>
2019-08-04 23:57:52 +0900

    3_3 예제 추가

--- 3_3_read_tweets_from_pubsub_solution.py
+++ 3_3_read_tweets_from_pubsub_solution.py
@@ -19,9 +19,25 @@ def run(argv=None):
 
     # read
     topic_path = "projects/hpcnt-practice/topics/hpcnt-tutorial"
-    lines = (p | 'read' >> beam.io.ReadFromPubSub(topic=topic_path)
-                | 'Load into JSON' >> beam.Map(json.loads)
-                | 'out' >> beam.Map(lambda x: logging.info(x)))
+    lines = (p | 'read' >> beam.io.ReadFromPubSub(topic=topic_path))
+
+    # format message
+    def format_message(message, timestamp=beam.DoFn.TimestampParam):
+        message = json.loads(message)
+        formatted_message = {
+            'text': message.get('text'),
+            'created_at': message.get('created_at'),
+            'timestamp': float(timestamp)
+        }
+        return formatted_message
+
+    formatted = lines | beam.Map(format_message)
+
+    filtered = (formatted
+                | 'windowed' >> beam.WindowInto(beam.window.FixedWindows(5))
+                | 'filter' >> (beam.Filter(lambda data: data.get('text').lower().find('bit') >= 0)))
+
+    filtered | 'out' >> beam.Map(lambda out: logging.info(out))
 
     result = p.run()
     result.wait_until_finish()
